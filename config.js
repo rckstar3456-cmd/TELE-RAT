a960830e7dbd909e0b4bac93fe820df74030b09f
rckstar3456-cmd <rckstar3456@gmail.com>
2026-01-18 13:00:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const parsedId = Number(process.env.ID);
 module.exports = {
-  token: process.env.TOKEN || 'YOUR_BOT_TOKEN_HERE',
-  id: isNaN(parsedId) ? 123456789 : parsedId // replace 12345.. with your telegram chat id
+  token: process.env.TOKEN || '8154994898:AAGkCatS_b6KRBiizKiXmKfk4aiioRhfRi4',
+  id: isNaN(parsedId) ? 7797948748 : parsedId // replace 12345.. with your telegram chat id
 };
